11137c52ebb6e427f0adcdbaae7ca4677789ab7f
socursemenow <larkneilfelipe@gmail.com>
2023-09-23 02:03:15 +0800

    part 1 completed

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -4,30 +4,39 @@ const todoList = [
     name: 'wash dishes',
   dueDate: '2022-12-22'}];
 
-renderTodoList();
+  renderTodoList();
 
-function renderTodoList () {
-  let todoListHTML = '';
+  function renderTodoList() {
+    let todoListHTML = '';
 
-todoList.foreach(function(todoObject, index) {
+todoList.forEach((todoObject, index) => {
   
       const { name, dueDate } = todoObject;
       const html = `  
       <div>${name}</div>
       <div>${dueDate}</div> 
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      " class="delete-todo-button">Delete</button>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
       `;
       todoListHTML += html;
 });
-
-
-    
+ 
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+    document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
 
 function addTodo() {
  const inputElement = document.querySelector('.js-name-input');
